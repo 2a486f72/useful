bae9f5c78223a34560c06eddeee74e14afce5f6d
2a <2a@486f72.com>
2013-03-24 13:46:16 +0200

    1.0.1 - Removed debug info from release build.

--- Useful/Properties/AssemblyInfo.cs
+++ Useful/Properties/AssemblyInfo.cs
@@ -3,8 +3,5 @@ using System.Resources;
 
 [assembly: AssemblyTitle("Useful")]
 [assembly: AssemblyCopyright("Copyright © 2a486f72 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
